7fd6632042fa84507236a686fbeda7193f380162
GabrielaBressa <gabybressa85@gmail.com>
2024-11-28 16:18:35 -0300

    salvar

--- portifolio2/portifolio.js
+++ portifolio2/portifolio.js
@@ -1,24 +1,20 @@
-// Função para exibir uma mensagem no console
+
 function exibirMensagem() {
     console.log("Bem-vindo ao portfólio de Gabriela Bressa! Explore meus projetos.");
 }
 
-// Função para mudar a cor de fundo ao clicar no título
 function mudarCorDeFundo() {
-    document.body.style.backgroundColor = "#a2dff7"; // Mudando a cor de fundo para um tom azul claro
+    document.body.style.backgroundColor = "#a2dff7"; 
 }
 
-// Função para alterar o estilo do botão ao passar o mouse
 function alterarEstiloBotao(botao) {
     botao.style.backgroundColor = "#4CAF50";
     botao.style.color = "white";
 }
 
-// Função para restaurar o estilo original do botão
 function restaurarEstiloBotao(botao) {
     botao.style.backgroundColor = "";
     botao.style.color = "";
 }
 
-// Quando a página carrega, exibe a mensagem no console
 window.onload = exibirMensagem;
